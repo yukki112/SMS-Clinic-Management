a3b7290ed3d0de0fd41540598195cb09a14a0e98
yukki112 <Stephenviray12@gmail.com>
2026-02-27 22:48:03 +0800

    main

--- api/clinic_requests.php
+++ api/clinic_requests.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 // Handle preflight OPTIONS request
@@ -29,8 +29,8 @@ class ClinicRequestsAPI {
             case 'GET':
                 $this->handleGet($endpoint);
                 break;
-            case 'PUT':
-                $this->handlePut($endpoint);
+            case 'POST':
+                $this->handlePost($endpoint);
                 break;
             case 'DELETE':
                 $this->handleDelete($endpoint);
@@ -61,7 +61,7 @@ class ClinicRequestsAPI {
         }
     }
     
-    private function handlePut($endpoint) {
+    private function handlePost($endpoint) {
         $data = json_decode(file_get_contents('php://input'), true);
         
         if (!$data) {
