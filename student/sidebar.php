b6b52893b36be87f79ab20092f8f2ea340d47754
yukki112 <Stephenviray12@gmail.com>
2026-03-01 22:14:39 +0800

    main

--- student/sidebar.php
+++ student/sidebar.php
@@ -102,28 +102,7 @@ $initials = substr($initials, 0, 2);
                             <span class="nav-label">Visit History</span>
                         </a>
                     </li>
-                    <li class="nav-item <?php echo $current_page == 'appointments.php' ? 'active' : ''; ?>">
-                        <a href="appointments.php" class="nav-link">
-                            <div class="nav-icon-wrapper">
-                                <svg class="nav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
-                                    <circle cx="12" cy="12" r="10"/>
-                                    <path d="M12 6V12L16 14"/>
-                                </svg>
-                            </div>
-                            <span class="nav-label">My Appointments</span>
-                        </a>
-                    </li>
-                    <li class="nav-item <?php echo $current_page == 'clearance_history.php' ? 'active' : ''; ?>">
-                        <a href="clearance_history.php" class="nav-link">
-                            <div class="nav-icon-wrapper">
-                                <svg class="nav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
-                                    <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999"/>
-                                    <path d="M22 4L12 14.01L9 11.01"/>
-                                </svg>
-                            </div>
-                            <span class="nav-label">Clearance History</span>
-                        </a>
-                    </li>
+                  
                 </ul>
             </div>
 
